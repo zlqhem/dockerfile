7216325d19447a89c50cb8ef63347b079502d312
Ubuntu <ubuntu@ip-172-31-16-129.us-west-2.compute.internal>
2015-03-08 01:26:46 +0000

    add data

---
+++ course-sdet/data/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# step 0. prepare findlib
+(cd findlib-1.5.5 && ./configure && make install)
+
+# step 0. prepare yices smt solver
+mkdir -p /home/jburnim
+cp -r yices /home/jburnim/yices-1.0.11
+
+# step 0. prepare crest source
+wget https://github.com/jburnim/crest/archive/v0.1.2.zip -O crest-v0.1.2.zip
+unzip crest-v0.1.2.zip
+
+# step 1. build crest-cil
+(cd crest-0.1.2/cil && ./configure && make)
+
+# step 1. build crest
+(cd crest-0.1.2/src && make)
+
+
